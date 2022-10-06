08d279c7c92dc9aa2d34fc0810989dde537ddefb
Toebzy <Tobiascarlsen@live.dk>
2022-10-06 14:31:54 +0200

    OOP1
    Package file

---
+++ TorsdagsOpgaver5/OOP1/src/Task2/Building.java
@@ -0,0 +1,13 @@
+package Task2;
+
+import java.util.ArrayList;
+
+public class Building {
+
+ArrayList<String> Room = new ArrayList<String>();
+
+private int numberOfBathrooms;
+private int numberOfFloors;
+private boolean isOfficeBuilding;
+
+}
